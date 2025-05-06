2d7b3f31708bec8189058452c7da37a1000dd381
анастасия <anastasia@MacBook-Air-anastasia-2.local>
2025-05-06 23:02:26 +0300

    feat: db

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"context"
+	"expense_tracker/internal/config"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Database struct {
+	Pool *pgxpool.Pool
+}
+
+func NewDB(ctx context.Context, cfg *config.Config) (*Database, error) {
+	pool, err := pgxpool.New(ctx, cfg.DBURL)
+	if err != nil {
+		return nil, fmt.Errorf("repository: unable to create connection pool: %w", err)
+	}
+	return &Database{Pool: pool}, nil
+}
+
+func (db *Database) Close() {
+	db.Pool.Close()
+}
