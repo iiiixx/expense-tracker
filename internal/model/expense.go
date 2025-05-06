63f564d5a92a1ff627694a0f78839e385db7a8ee
анастасия <anastasia@MacBook-Air-anastasia-2.local>
2025-05-06 22:59:14 +0300

    feat: create models

--- internal/model/expense.go
+++ internal/model/expense.go
@@ -1,1 +1,19 @@
 package model
+
+import "time"
+
+type Expense struct {
+	ID          int       `json:"id"`
+	UserID      int       `json:"user_id"`
+	Amount      float64   `json:"amount"`
+	Category    string    `json:"category"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+}
+
+type UpdateExpenseInput struct {
+	Amount      *float64   `json:"amount,omitempty"`
+	Category    *string    `json:"category,omitempty"`
+	Description *string    `json:"description,omitempty"`
+	Date        *time.Time `json:"date,omitempty"`
+}
