e1c6e3ffcbee67f5663489b99e70d23519404834
анастасия <anastasia@MacBook-Air-anastasia-2.local>
2025-05-09 00:26:58 +0300

    feat: UserHandler

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,6 +1,11 @@
 package handler
 
-import "expense_tracker/internal/service"
+import (
+	"encoding/json"
+	"expense_tracker/internal/model"
+	"expense_tracker/internal/service"
+	"net/http"
+)
 
 type UserHandler struct {
 	userService *service.UserService
@@ -10,4 +15,47 @@ func NewUserHandler(userService *service.UserService) *UserHandler {
 	return &UserHandler{
 		userService: userService,
 	}
+}
+
+func (h *UserHandler) UpdateUsername(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(int)
+
+	var input model.UpdateUsernameInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, `{"error": "Invalid request body"}`, http.StatusBadRequest)
+		return
+	}
+
+	updatedUser, err := h.userService.UpdateUsername(r.Context(), userID, &input)
+	if err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedUser)
+}
+
+func (h *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(int)
+
+	if err := h.userService.DeleteUser(r.Context(), userID); err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *UserHandler) GetProfile(w http.ResponseWriter, r *http.Request) {
+	userID := r.Context().Value("userID").(int)
+
+	user, err := h.userService.GetUserProfile(r.Context(), userID)
+	if err != nil {
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
 }
